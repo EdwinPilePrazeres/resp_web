c17f5ef336806f3165eaf6260fa17af86f985fcd
Edwin Pile <69055818+EdwinPilePrazeres@users.noreply.github.com>
2021-03-25 04:22:28 -0500

    master

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HeroSection = () => {
+    return (
+        <HeroContainer id='home8'>
+            
+        </HeroContainer>
+    )
+}
+
+export default HeroSection
